952793487d82ce5ea955be209989485a37409722
George Yildiz <george.yildiz@gmail.com>
2018-03-28 16:59:48 +0200

    Add route for search movies

--- modules/movies/server/routes/movies.server.routes.js
+++ modules/movies/server/routes/movies.server.routes.js
@@ -1,6 +1,10 @@
 'use strict';
 
+var moviesController = require('../controllers/movies.server.controller');
+
 module.exports = function (app) {
   // Routing logic
   // ...
+  app.route('/api/movies/search/:query')
+    .get(moviesController.search);
 };
