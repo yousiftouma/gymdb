43be9c4dae9d45b2c756c0d188c4f4787cda12f5
George Yildiz <george.yildiz@gmail.com>
2018-04-17 17:11:27 +0200

    Use usermovieinfo resolve in controller

--- modules/movies/client/controllers/movie-details.client.controller.js
+++ modules/movies/client/controllers/movie-details.client.controller.js
@@ -5,13 +5,16 @@
     .module('movies')
     .controller('MovieDetailsController', MovieDetailsController);
 
-  MovieDetailsController.$inject = ['moviesService', 'movieResolve', 'posterConfig', 'Authentication', '$anchorScroll', '$location'];
+  MovieDetailsController.$inject = ['moviesService', 'movieResolve', 'userMovieInfoResolve', 'posterConfig',
+    'Authentication', '$anchorScroll', '$location'];
 
-  function MovieDetailsController(moviesService, movieResolve, posterConfig, authentication, $anchorScroll, $location) {
+  function MovieDetailsController(moviesService, movieResolve, userMovieInfoResolve, posterConfig,
+                                  authentication, $anchorScroll, $location) {
     let vm = this;
     vm.postComment = postComment;
     vm.baseImagePath = posterConfig.imageBaseUrl + posterConfig.posterSizes.xl;
     vm.movie = movieResolve.data;
+    vm.userMovieInfo = userMovieInfoResolve.data;
     vm.user = authentication.user;
     init();
 
