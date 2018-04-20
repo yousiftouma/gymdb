9b292c5fef66a84ebb369f60817a72249b4f1447
George Yildiz <george.yildiz@gmail.com>
2018-04-20 14:59:22 +0200

    Move resolve to child state

--- modules/mypage/client/config/mypage.client.routes.js
+++ modules/mypage/client/config/mypage.client.routes.js
@@ -12,19 +12,12 @@
     // Mypage state routing
     $stateProvider
       .state('mypage', {
+        abstract: true,
         url: '/mypage',
-        templateUrl: '/modules/mypage/client/views/mypage.client.view.html',
-        controller: 'MypageController',
-        controllerAs: 'vm',
-        data: {
-          roles: ['users', 'admin']
-        },
-        resolve: {
-          myPageResolve: getMyPage
-        }
+        templateUrl: '/modules/mypage/client/views/mypage.client.view.html'
       })
       .state('mypage.watch', {
-        url: '/watch',
+        url: '',
         templateUrl: '/modules/mypage/client/views/mypage-watch.client.view.html',
         controller: 'MypageWatchController',
         controllerAs: 'vm',
@@ -32,20 +25,41 @@
           roles: ['users', 'admin']
         },
         resolve: {
+          myPageResolve: getMyPage,
           watchlistResolve: getWatchlistMovies
+        }
+      })
+      .state('mypage.seen', {
+        url: '',
+        templateUrl: '/modules/mypage/client/views/mypage-seen.client.view.html',
+        controller: 'MypageSeenController',
+        controllerAs: 'vm',
+        data: {
+          roles: ['users', 'admin']
         }
       });
 
     getMyPage.$inject = ['$stateParams', 'mypageService'];
-    getWatchlistMovies.$inject = ['$stateParams', 'mypageService', 'myPageResolve'];
+    getWatchlistMovies.$inject = ['$stateParams', 'moviesService', 'myPageResolve'];
 
     function getMyPage($stateParams, mypageService) {
       return mypageService.getMyPage();
     }
 
-    function getWatchlistMovies($stateParams, mypageService, myPageResolve) {
+    function getWatchlistMovies($stateParams, moviesService, myPageResolve) {
       let moviesToGet = myPageResolve.data.watchlist.slice(0, 10);
-      return mypageService.getMovies(moviesToGet);
+      return moviesService.getMovies(moviesToGet).then((result) => {
+        console.log(result);
+        // Append information regarding whether the movie is seen by the user
+        let hej = result.data.map((movie) => {
+          movie.isSeen = myPageResolve.data.seenMovies.filter((seenMovie) => {
+            return movie.id === seenMovie.tmdbId;
+          }).length > 0;
+          return movie;
+        });
+        console.log(hej);
+        return hej;
+      });
     }
   }
 }());
