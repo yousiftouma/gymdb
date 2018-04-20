f6095e5973a17b16ac871c8a751e2464d0d94ce3
George Yildiz <george.yildiz@gmail.com>
2018-04-20 14:51:15 +0200

    Add buttons in watchlist state

--- modules/mypage/client/controllers/mypage-watch.client.controller.js
+++ modules/mypage/client/controllers/mypage-watch.client.controller.js
@@ -1,17 +1,18 @@
-(function() {
+(function () {
   'use strict';
 
   angular
     .module('mypage')
     .controller('MypageWatchController', MypageWatchController);
 
-  MypageWatchController.$inject = ['$scope', 'myPageResolve', 'watchlistResolve'];
+  MypageWatchController.$inject = ['$scope', 'myPageResolve', 'watchlistResolve', 'posterConfig', 'mypageService'];
 
-  function MypageWatchController($scope, myPageResolve, watchlistResolve) {
+  function MypageWatchController($scope, myPageResolve, watchlistResolve, posterConfig, mypageService) {
     let vm = this;
-    console.log(watchlistResolve);
-    console.log(myPageResolve);
-
+    vm.movies = watchlistResolve;
+    vm.baseImagePath = posterConfig.imageBaseUrl + posterConfig.posterSizes.xl;
+    vm.removeFromWatchlist = removeFromWatchlist;
+    vm.updateSeenList = updateSeenList;
     // Mypage controller logic
     // ...
 
@@ -19,5 +20,31 @@
 
     function init() {
     }
+
+    function removeFromWatchlist(movieId) {
+      const data = { tmdbId: movieId, delete: true };
+      mypageService.updateWatchlist(data).then(() => {
+        vm.movies = vm.movies.filter((movie) => {
+          return movie.id !== movieId;
+        });
+      }, handleError);
+    }
+
+    function updateSeenList(movieId, isSeen) {
+      const data = { tmdbId: movieId, delete: isSeen };
+      mypageService.updateSeenList(data).then(() => {
+        vm.movies = vm.movies.map((movie) => {
+          if (movie.id === movieId) {
+            movie.isSeen = !isSeen;
+          }
+          return movie;
+        });
+      });
+    }
+
+    function handleError(error) {
+      console.log('Logging error');
+      console.log(error);
+    }
   }
-})();
+}());
