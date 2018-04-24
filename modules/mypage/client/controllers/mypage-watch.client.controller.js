b95a54664bcb8c0fc7ab27a5ac448103a6631274
George Yildiz <george.yildiz@gmail.com>
2018-04-24 10:51:15 +0200

    Add pagination functionality to watchlist

--- modules/mypage/client/controllers/mypage-watch.client.controller.js
+++ modules/mypage/client/controllers/mypage-watch.client.controller.js
@@ -5,10 +5,17 @@
     .module('mypage')
     .controller('MypageWatchController', MypageWatchController);
 
-  MypageWatchController.$inject = ['$scope', 'myPageResolve', 'watchlistResolve', 'posterConfig', 'mypageService'];
+  MypageWatchController.$inject = ['$scope', 'myPageResolve', 'watchlistResolve', 'posterConfig',
+    'mypageService', 'moviesService', '$anchorScroll'];
 
-  function MypageWatchController($scope, myPageResolve, watchlistResolve, posterConfig, mypageService) {
+  function MypageWatchController($scope, myPageResolve, watchlistResolve, posterConfig, mypageService, moviesService,
+                                 $anchorScroll) {
     let vm = this;
+    vm.currentPage = 1;
+    vm.previousPage = previousPage;
+    vm.finalPage = false;
+    vm.nextPage = nextPage;
+    vm.moviesOnWatchlist = myPageResolve.data.watchlist.length;
     vm.movies = watchlistResolve;
     vm.baseImagePath = posterConfig.imageBaseUrl + posterConfig.posterSizes.xl;
     vm.removeFromWatchlist = removeFromWatchlist;
@@ -45,6 +52,45 @@
     function handleError(error) {
       console.log('Logging error');
       console.log(error);
+    }
+
+    function previousPage() {
+      vm.currentPage = vm.currentPage - 1;
+      let endIndex = vm.currentPage * 10;
+      let moviesToGet = myPageResolve.data.watchlist.slice(endIndex - 10, endIndex);
+      getWatchlistMovies(moviesToGet).then((result) => {
+        vm.movies = result;
+        vm.finalPage = false;
+        $anchorScroll();
+      });
+    }
+
+    function nextPage() {
+      vm.currentPage = vm.currentPage + 1;
+      let endIndex = vm.currentPage * 10;
+      let moviesToGet = myPageResolve.data.watchlist.slice(endIndex - 10, endIndex);
+      getWatchlistMovies(moviesToGet).then((result) => {
+        vm.movies = result;
+        if (vm.movies.length < 10) {
+          vm.finalPage = true;
+        }
+        $anchorScroll();
+      });
+    }
+
+    function getWatchlistMovies(moviesToGet) {
+      return moviesService.getMovies(moviesToGet).then((result) => {
+        // Append information regarding whether the movie is seen by the user
+        if (!result.data) {
+          return [];
+        }
+        return result.data.map((movie) => {
+          movie.isSeen = myPageResolve.data.seenMovies.filter((seenMovie) => {
+            return movie.id === seenMovie.tmdbId;
+          }).length > 0;
+          return movie;
+        });
+      });
     }
   }
 }());
