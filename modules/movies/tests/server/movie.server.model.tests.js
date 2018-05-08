4e56f0172840e21ed02f576ce3ffecc429fd0cb4
George Yildiz <george.yildiz@gmail.com>
2018-05-08 13:12:42 +0200

    Add a failing test

--- modules/movies/tests/server/movie.server.model.tests.js
+++ modules/movies/tests/server/movie.server.model.tests.js
@@ -17,8 +17,8 @@ var user,
 /**
  * Unit tests
  */
-describe('Movie Model Unit Tests:', function() {
-  beforeEach(function(done) {
+describe('Movie Model Unit Tests:', function () {
+  beforeEach(function (done) {
     user = new User({
       firstName: 'Full',
       lastName: 'Name',
@@ -28,7 +28,7 @@ describe('Movie Model Unit Tests:', function() {
       password: 'password'
     });
 
-    user.save(function() {
+    user.save(function () {
       movie = new Movie({
         // Add model fields
         // ...
@@ -39,16 +39,21 @@ describe('Movie Model Unit Tests:', function() {
     });
   });
 
-  describe('Method Save', function() {
-    it('should be able to save without problems', function(done) {
-      movie.save(function(err) {
+  describe('Method Save', function () {
+    it('should be able to save without problems', function (done) {
+      movie.save(function (err) {
         should.not.exist(err);
         done();
       });
     });
+    it('should fail', function (done) {
+      movie.save(function (err) {
+        done(new Error('fail on purpose'));
+      });
+    });
   });
 
-  afterEach(function(done) {
+  afterEach(function (done) {
     Movie.remove().exec();
     User.remove().exec();
 
